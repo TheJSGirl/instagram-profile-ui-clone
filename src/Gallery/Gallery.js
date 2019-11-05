ea536503114de77ae41e469add9636383ef3405c
TheJSGirl <prinegi100@gmail.com>
2019-11-05 23:46:39 +0530

    feat: added Tablink

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -16,13 +16,31 @@ const PhotoGrid = styled.div`
   gap:20px;
 `;
 
+const LinkGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 20px;
+`;
+const TabLink = styled(Link)`
+  text-decoration: none;
+  color: #ccc;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  ${({selected}) => selected && 'color: black;'}
+`;
 
 export function Gallery() {
-    let location = useLocation();
-  
+    let location = useLocation();  
+    const cascade = location.search === '?type=cascade';
     return (
       <div>
         <UserGrid />
+        <LinkGrid>
+            <TabLink selected={!cascade} to={{pathname: `${location.pathname}`, search:"?type=square"}} >Square</TabLink>
+            <TabLink selected={cascade} to={{pathname: `${location.pathname}`, search:"?type=cascade"}}>Cascade</TabLink>
+        </LinkGrid>
         <PhotoGrid>
           {Posts.map(i => (
             <Link
